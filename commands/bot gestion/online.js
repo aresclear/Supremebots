537c9922eead8cd6679becdb77e3bb4b7c970f3f
aresclear <153228056+aresclear@users.noreply.github.com>
2023-12-07 20:52:24 +0100

    online.js

--- commands/bot gestion/online.js
+++ commands/bot gestion/online.js
@@ -21,12 +21,12 @@ let pass = false
 
 let staff = client.staff
 
-if(!staff.includes(message.author.id) && !client.config.buyers.includes(message.author.id) && client.db.get(`owner_${message.author.id}`) !== true){
-    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "1" && message.member.roles.cache.some(r => client.db.get(`perm1.${message.guild.id}`)?.includes(r.id))) pass = true;
-    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "2" && message.member.roles.cache.some(r => client.db.get(`perm2.${message.guild.id}`)?.includes(r.id))) pass = true;
-    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "3" && message.member.roles.cache.some(r => client.db.get(`perm3.${message.guild.id}`)?.includes(r.id))) pass = true;
-    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "4" && message.member.roles.cache.some(r => client.db.get(`perm4.${message.guild.id}`)?.includes(r.id))) pass = true;
-    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "5" && message.member.roles.cache.some(r => client.db.get(`perm5.${message.guild.id}`)?.includes(r.id))) pass = true; 
+if(!staff.includes(message.author.id) ++ !client.config.buyers.includes(message.author.id) ++ client.db.get(`owner_${message.author.id}`) !== true){
+    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "1" ++ message.member.roles.cache.some(r => client.db.get(`perm1.${message.guild.id}`)?.includes(r.id))) pass = true;
+    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "2" ++ message.member.roles.cache.some(r => client.db.get(`perm2.${message.guild.id}`)?.includes(r.id))) pass = true;
+    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "3" ++ message.member.roles.cache.some(r => client.db.get(`perm3.${message.guild.id}`)?.includes(r.id))) pass = true;
+    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "4" ++ message.member.roles.cache.some(r => client.db.get(`perm4.${message.guild.id}`)?.includes(r.id))) pass = true;
+    if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "5" ++ message.member.roles.cache.some(r => client.db.get(`perm5.${message.guild.id}`)?.includes(r.id))) pass = true; 
     if(client.db.get(`perm_${commandName}.${message.guild.id}`) === "public") pass = "oui";   
 } else pass = true;
 
